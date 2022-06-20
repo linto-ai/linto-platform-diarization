862c675259b10f310bc71aba3dd525d5c4232e5b
wghezaiel <wghezaiel@linagora.com>
2022-06-20 09:59:03 +0200

    modification data_preparation_kaldi

--- egs/data_preparation_kaldi.py
+++ egs/data_preparation_kaldi.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
-# This script is called by local/make_dihard_2018_eval.sh, and it creates the
-# necessary files for DIHARD 2018 evaluation directory.
 
 import sys, os
 import re
-def prepare_dihard_2018_eval(src_dir, data_dir):
-    #wavscp_fi = open(data_dir + "/wav.scp" , 'w')
-    #utt2spk_fi = open(data_dir + "/utt2spk" , 'w')
-    #segments_fi = open(data_dir + "/segments" , 'w')
-    #rttm_fi = open(data_dir + "/rttm" , 'w')
-    #reco2num_spk_fi = open(data_dir + "/reco2num_spk" , 'w')
+def prepare_data(src_dir, data_dir):
+    
     with open(os.path.join(data_dir, "wav.scp"), "w") as fout:
 		    for speaker in sorted(os.listdir(src_dir)):
 		        print(speaker)
@@ -33,7 +27,7 @@ def main():
     data_dir = sys.argv[2]
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
-    prepare_dihard_2018_eval(src_dir, data_dir)
+    prepare_data(src_dir, data_dir)
     return 0
 
 if __name__=="__main__":
